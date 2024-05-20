d2c70dd548b37589fd022fa04fc8d0f666ea217b
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-05-20 23:08:38 +0000

    Filtered message

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,2 @@
+## 0x00. Personal data
+### Learning about logging, PII, non-PII, and Personal Data
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module contains a function called filter_datum
+that returns the log message obfuscated.
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Returns the log message obfuscated based on the provided fields."""
+    for field in fields:
+        message = re.sub(fr'{field}=[^{separator}]*', f'{field}={redaction}', message)
+    return message
