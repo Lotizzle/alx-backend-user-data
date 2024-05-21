da07dcf9fd5f4a35df2d4035e4c69c415550d35c
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-05-21 23:48:13 +0000

    401 error handling

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, Blueprint, abort
 from api.v1.views import app_views
+
+app_views = Blueprint('app_views', __name__)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -24,3 +26,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_route() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - the error handling for 401
+    """
+    abort(401)
