79f5df8cf664e82e8c31d1f75e5f4625a4be4f85
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-05-24 22:31:55 +0100

    Requires auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if path requires authentication """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and excluded_path == path:
+                return False
+        
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves the authorization header from the request. """
