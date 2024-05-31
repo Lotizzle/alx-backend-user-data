d6d1bd42a9ffbb9bc16f323e3c01ed4b0b25645a
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-01 00:06:41 +0100

    Logging in

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,6 +46,9 @@ class Auth:
         """Check if login credentials are valid."""
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8'))
+            return bcrypt.checkpw(
+                    password.encode('utf-8'),
+                    user.hashed_password.encode('utf-8')
+            )
         except NoResultFound:
             return False
