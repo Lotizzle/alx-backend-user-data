fd41c42858c8ddd7398afb69d733729ad13bc567
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-05-31 04:48:16 +0000

    Found user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 DB module that adds users
 """
@@ -5,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,3 +40,16 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyword arguments
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except NoResultFound:
+            raise NoResultFound("No user found with the given arguments")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments provided")
