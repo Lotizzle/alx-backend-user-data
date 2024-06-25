632dea0ab8854c59aa79fec7019e533a9f60191e
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-06-25 11:41:11 +0100

    BasicAuth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,6 +6,8 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 import os
 
 
@@ -16,8 +18,9 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
-if AUTH_TYPE == 'auth':
-    from api.v1.auth.auth import Auth
+if AUTH_TYPE == 'basic_auth':
+    auth = BasicAuth()
+else:
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module contains the Basic Auth Class. """
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    pass
