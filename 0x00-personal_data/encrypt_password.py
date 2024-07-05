e0c4b6271b25dca772a0e2048f4917dc7ba4704c
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-07-05 15:17:35 +0100

    Pycode

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -28,6 +28,7 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
         password (str): The plain text password to validate.
 
     Returns:
-        bool: True if the password matches the hashed password, False otherwise.
+        bool: True if the password matches the hashed password,
+        False otherwise.
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
